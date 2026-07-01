3d99881b1eae3aa4f0a451b10c0dda8f52f965a0
hasbihasbullh <mhasbihasbullah@gmail.com>
2026-07-02 00:10:07 +0700

    remove explicit any to resolve build error in layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -67,7 +67,7 @@ export default async function RootLayout({
 }) {
   const { locale } = await params;
 
-  if (!routing.locales.includes(locale as any)) {
+  if (!routing.locales.includes(locale as "en" | "id")) {
     notFound();
   }
 
